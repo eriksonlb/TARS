b32d2b11e6de25c1874db6f47668b42de3940dc7
Erikson Lopes <elbezerra@topaz.com.br>
2021-02-13 15:22:52 -0300

    adicionando leitura e escrita de acentos no json

--- helpers/data.py
+++ helpers/data.py
@@ -2,14 +2,14 @@ import json
 
 def assistent_data():
     try:
-        with open("data/assistent_personality.json") as json_file:
+        with open("data/assistent_personality.json", "r", encoding="utf8") as json_file:
             json.load(json_file)
             json_file.close()
 
     except FileNotFoundError:
         header = 'personality'
         header = header.replace("'", "")
-        with open("data/assistent_personality.json", "w") as json_file:
+        with open("data/assistent_personality.json", "w", encoding="utf8") as json_file:
             json.dump(header, json_file)
             json_file.close()
             
