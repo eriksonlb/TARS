ff921367628de0124aedd9b0c8f39bd1c5f64f4a
Erikson Lopes <elbezerra@topaz.com.br>
2021-02-14 01:11:29 -0300

    adicionadno som de iniciar e encerrar e alterando som de fala

--- sound.py
+++ sound.py
@@ -3,5 +3,13 @@ import ipdb
 from time import sleep
 
 def speak_sound():
-    playsound('src\ok.mp3')
+    playsound('src\ok.wav')
+    sleep(0.3)
+
+def start_sound():
+    playsound('src\start.wav')
+    sleep(0.3)
+
+def end_sound():
+    playsound('src\end.wav')
     sleep(0.3)
