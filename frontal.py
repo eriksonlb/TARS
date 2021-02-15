c67df5b2f07c0c8f9853c8ed4b8cff38889f284b
Erikson Lopes <elbezerra@topaz.com.br>
2021-02-14 23:41:44 -0300

    criando machine learning mais rápido (clima não funciona)

---
+++ frontal.py
@@ -0,0 +1,36 @@
+import json
+import math
+import random
+import ipdb
+
+
+def get_response(text):
+    with open("data/intents.json", 'r', encoding='utf8') as file:
+        data = json.load(file)
+        intents = data['intents']
+        bag_words = []
+        max_acc = 0
+        for intent in intents:
+            inputs = intent['patterns']
+            tag = intent['tag']
+            for input in inputs:
+                input_bag = []
+                phrase = input.split(" ")
+                try:
+                    phrase.remove('')
+                except:
+                    pass
+                for word in phrase:
+                    input_bag.append(1) if word in text else 0
+                value = sum(input_bag)
+                n_words = len(text.split(" "))
+                accuracy = (value * 100) / n_words
+                label = f"{tag} - {input} - {accuracy}%"
+                print(label)
+                if accuracy > max_acc:
+                    max_acc = accuracy
+                    response_tag = tag
+                    response = intent['responses']
+        ipdb.sset_trace()
+        return {"response": random.choice(response),
+                "tag": response_tag}
